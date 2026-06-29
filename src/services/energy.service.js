bb273604b9689da6fdfc7a30ed829b0015fe879a
Junaid <junaidshaikh141003@gmail.com>
2026-06-29 17:11:41 +0530

    added redis caching

--- src/services/energy.service.js
+++ src/services/energy.service.js
@@ -1,28 +1,58 @@
 const energyMock = require('../mock/energy.mock');
 
+const redis = require('../cache/redis');
+
+const CACHE_TTL = 60; 
+
 const getEnergyConsumption = async(machineIds, startDate, endDate) => {
     if(!machineIds || !Array.isArray(machineIds) || machineIds.length === 0){
-        const error = new error('Atleast one machineid is required');
-        error.statuscode = 400;
+        const error = new Error('Atleast one machineid is required');
+        error.statusCode = 400;
         throw error;
     }
     if (!startDate || !endDate) {
-        const error = new error('startDate and endDate are required')
-        error.statuscode = 400;
+        const error = new Error('startDate and endDate are required')
+        error.statusCode = 400;
         throw error;
     }
 
-    return energyMock.getConsumptionData(machineIds, startDate, endDate)
-} 
+    const cacheKey = `energy:consumption:${machineIds.join(',')}:${startDate}:${endDate}`;
 
+    const cached = await redis.get(cacheKey);
+    if (cached){
+        console.log('Cache hit: ', cacheKey)
+        return JSON.parse(cached)
+    }
+
+    console.log('Cache miss:',  cacheKey)
+    const data =  energyMock.getConsumptionData(machineIds, startDate, endDate)
+
+    await redis.setex(cacheKey, CACHE_TTL, JSON.stringify(data));
+
+    return data
+}
+    
 const getMachineEnergyData = async(machineId) => {
     if(!machineId) {
-        const error = new error('Machine Id is required');
-        error.statuscode = 400;
+        const error = new Error('Machine Id is required');
+        error.statusCode = 400;
         throw error;
     }
 
-    return energyMock.getMachineData(machineId)
+    const cacheKey = `energy:machine:${machineId}`;
+
+    const cached = await redis.get(cacheKey);
+    if (cached) {
+        console.log('Cache hit: ', cacheKey)
+        return JSON.parse(cached)
+    }
+
+    console.log('Cache miss: ', cacheKey);
+    const data = energyMock.getMachineData(machineId);
+    await redis.setex(cacheKey, CACHE_TTL, JSON.stringify(data));
+
+    return data;
+
 }
 
 module.exports = { getEnergyConsumption, getMachineEnergyData }
